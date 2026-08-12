860b814835db5a3a12e8779ae98e3cc737ad8b05
dafaak <israel.pe@outlook.es>
2026-08-12 15:56:16 -0500

    workflow added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://dafaak.github.io",
+  base: "/talabarteria-perez/",
   vite: {
     plugins: [tailwindcss()]
   }
